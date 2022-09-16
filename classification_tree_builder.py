6b8923c2a132b2a4aa694aa4a1f9bd92a389a85d
ThierryOrth <thierry.orth@gmail.com>
2022-09-16 07:57:46 +0200

    changed name of classification tree builder file

---
+++ classification_tree_builder.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+class Tree:
+    pass
+
+def tree_grow(x, y, nmin, minleaf, nfeat):
+    pass
+
+def tree_pred():
+    pass
+
+def tree_grow_b():
+    pass
+
+def tree_pred_b():
+    pass
