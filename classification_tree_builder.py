8ad15ed5c4868d55bac2036747182159d4e63260
ThierryOrth <thierry.orth@gmail.com>
2022-09-20 17:17:36 +0200

    Updated

--- classification_tree_builder.py
+++ classification_tree_builder.py
@@ -1,27 +1,20 @@
 import numpy as np
+from data_loader import CREDIT_DATA
+from tree_structures import Node, Tree
 import collections
-import sys
-from data_loader import *
-from tree_structures import *
 
-    # FOR EACH NODE, PROVIDE A SPLIT THRESHOLD USING A DICTIONARY MAPPING
-
-    # FOR EACH NODE, PROVIDE A DICTIONARY THAT MAPS ATTRIBUTE VALUES TO PREDICTION 
-
-    # IF LEAF NODE IS REACHED, MAJORITY CLASS IS ASSIGNED BY TAKING THE KEY WHICH HAS THE MAXIMUM VALUE
 
 ATTRIBUTES = {0:"age", 1:"married", 2:"house", 3:"income", 4:"gender", 5:"class"}
-ATTRIBUTE_MAP = {0:0, 1:1, 2:2, 3:3, 4:4}
 
-def getimpurity(y):
+def impurity(y):
     total = len(y)
     p_zeros = len(y[y[:] == 0])/total
     p_ones = len(y[y[:]==1])/total
     return p_zeros*p_ones
 
 def bestsplit(x,y):
-    lowest_impurity = 1
-    best_split = -np.infty
+    lowest_imp = 1 # OUTER BOUNDARY OF GINI INDEX RANGE
+    best_split = -np.infty #
         
     x_sorted = np.sort(np.unique(x))
     candidate_splits = (x_sorted[0:-1]+x_sorted[1:len(x)])/2
@@ -30,42 +23,32 @@ def bestsplit(x,y):
         left_child = y[x <= split]
         right_child = y[split < x]
 
-        i_left = getimpurity(left_child)
-        i_right = getimpurity(right_child)
+        i_left = impurity(left_child)
+        i_right = impurity(right_child)
         pi_left = len(left_child)/len(x)
         pi_right = len(right_child)/len(x)
 
-        impurity = (pi_left * i_left) + (pi_right * i_right)
+        imp = (pi_left * i_left) + (pi_right * i_right)
 
-        if impurity < lowest_impurity:
-            lowest_impurity = impurity
+        if imp < lowest_imp:
+            lowest_imp = imp
             best_split = split
 
     return best_split
 
 def tree_grow(x, y, nmin, minleaf, nfeat):
-    tree = Tree()
+    root_node = Node()
+    extend_node(root_node, x, y, nmin, minleaf, nfeat)
+    return Tree(root_node)
 
-    if 1<len(x.shape):
-        _ , m = x.shape
-        
-    else:
-        m = len(x)
-        nodes = None
+def extend_node(node, x, y, nmin, minleaf, nfeat):
+    _, num_of_obs = x.shape
 
-    print(f"x:{x}, \n y:{y}")
-        
-    if nmin<m:
-
-        if 0<getimpurity(y):
-            index = np.random.choice(np.arange(0,m,1))
-            feature_values = x[:, index]
+    if nmin<=num_of_obs:
+        if 0<impurity(y):
+            feature_index = np.random.choice(np.arange(0,num_of_obs,1))
+            feature_values = x[:, feature_index]
             best_split = bestsplit(feature_values, y)
-
-            print(best_split)
-
-            tree.set_threshold(best_split)
-            tree.feature_value = index
 
             left = feature_values[feature_values <= best_split]
             right = feature_values[best_split < feature_values]
@@ -73,69 +56,26 @@ def tree_grow(x, y, nmin, minleaf, nfeat):
             left_labels = y[feature_values <= best_split]
             right_labels = y[best_split < feature_values]
 
-            if len(left)<minleaf or len(right)<minleaf:
-                feature_value = 1 if len(right_labels)<len(left_labels) else 0
-                tree.feature_value = feature_value           
+            node.feature_value = feature_index
+            node.split_threshold = best_split
+
+            node.left_child = Node()
+            node.right_child = Node()
+
+            print(f"\n left : {left} \n right : {right} \n left_labels : {left_labels} \n right_labels : {right_labels} \n feature_index : {feature_index} \n split : {best_split} \n")
+
+            if minleaf<=len(left) and minleaf<=len(right):
+                extend_node(node.left_child, x[feature_values<=best_split], left_labels, nmin, minleaf, nfeat)
+                extend_node(node.right_child, x[best_split<feature_values], right_labels, nmin, minleaf, nfeat)
             else:
-                # next nodes should be connected to previous nodes
-                tree_grow(x,y,nmin,minleaf,nfeat)
-                tree_grow(x,y,nmin,minleaf,nfeat)
+                node.left_child.is_leaf = node.right_child.is_leaf = True
+                node.left_child.feature_value = 1 # TODO: ASSIGN MAJORITY CLASS
+                node.right_child.feature_value = 1 # TODO: ASSIGN MAJORITY CLASS 
 
-        
-            print(f"x:{x} \n, y:{y}, \n feature index:{index}, \n split:{best_split}, \n left:{left},\n right:{right}")
+    node.is_leaf = True
+    node.feature_value = 1
 
-        
-    return tree
-
-
-
-
-            
-
-
-
-
-
-    # TODO: CONSTRUCT TREE OBJECT
-    # TODO: MAKE LEAF NODES IF CONTAIN ONLY SINGLE OBSERVATION
-    # TODO: NMIN, MINLEAF, NFEAT
-
-
-    # while 0<len(nodes):
-    #     
-    #     current_node = nodes[index]
-    #     node_labels = labels[index]
-    #     print("INDEX", index)
-    #     del nodes[index]
-    #     del labels[index]
-    #     m=len(nodes)
-
-    #     if 0<getimpurity(node_labels):
-    #         best_split = bestsplit(current_node, node_labels)
-
-    #         print(f"SPLIT: {best_split}")
-            
-    #         left_child = current_node[current_node <= best_split]
-    #         right_child = current_node[best_split < current_node]
-
-    #         left_child_labels = node_labels[current_node <= best_split]
-    #         right_child_labels = node_labels[best_split < current_node]
-
-
-
-    #         nodes.append(left_child)
-    #         nodes.append(right_child)
-    #         labels.append(left_child_labels)
-    #         labels.append(right_child_labels)
-
-    #         # tree_grow(left)
-    #         # tree_grow(right)
-
-    #         print(f"BEST SPLIT: {best_split}, \n\n LEFT CHILD WITH PREDICTION: {left_child, left_child_labels}, \
-    #                                     \n\n RIGHT CHILD WITH PREDICTION: {right_child, right_child_labels}, \
-    #                                         \n\n UPDATED NODE LIST: {nodes}, \n\n UPDATED LABELS: {labels}")
-
-    return 
+    
 
 def tree_pred(x, tr):
     pass
@@ -152,13 +92,12 @@ def tree_pred_b(x, trs):
 
 
 if __name__ == "__main__":
-    bestsplit(CREDIT_DATA[:,3],CREDIT_DATA[:,5])
+    #bestsplit(CREDIT_DATA[:,3],CREDIT_DATA[:,5])
 
-    # n, m = CREDIT_DATA.shape
-    # data = [elem for elem in CREDIT_DATA[:n,:]]
-    # labels = CREDIT_DATA[:,5]
-    # print(data)
-    # print(labels)
-    tree_grow(x=CREDIT_DATA[:,:5], y=CREDIT_DATA[:,5], nmin = 1, minleaf = 1, nfeat = 1)
-    # tree_grow(x=credit_data[:,:5],y=credit_data[:,5], nmin = 15, minleaf = 5, nfeat = 41)
+    X = CREDIT_DATA[:,:5] 
+    Y = CREDIT_DATA[:,5]
+    tree = tree_grow(x=X, y=Y, nmin = 1, minleaf = 1, nfeat = 1)
+    #tree.node_repr(current=tree.root) #TODO: ADD INDICES
+    tree_pred(x=X,tr=tree)
+   
   
--- tree_structures.py
+++ tree_structures.py
@@ -1,20 +1,27 @@
-class Tree:
+class Node:
     def __init__(self):
+        #self.node_index = node_index
         self.feature_value = None
-        self.threshold = None
+        self.split_threshold = None
         self.left_child = None
         self.right_child = None
+        self.is_leaf = False
 
-    def set_children(self, left_child, right_child):
-        self.left_child = left_child
-        self.right_child = right_child
+class Tree:
+    def __init__(self, root):
+        if isinstance(root,Node):
+            self.root = root
+        else:
+            raise ValueError("")
+        
+    def node_repr(self, current):
+        if current:
+            print(f"index of node : {None} \n\t feature : {current.feature_value} \
+                                \n\t\t threshold : {current.split_threshold} \
+                                \n\t\t\t leaf node? {current.is_leaf}")
+                                # \n\t\t child node indices : {current.left_child.node_index if current.left_child else None, current.right_child.node_index if current.right_child else None} \
+                                
+            self.node_repr(current.left_child)
+            self.node_repr(current.right_child)
 
-    def set_threshold(self, threshold):
-        self.threshold = self.threshold 
 
-# class Tree:
-#     def __init__(self, root):
-#         if isinstance(root,Node):
-#             self.root = root
-#         else:
-#             raise ValueError("")
