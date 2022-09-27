b400091ef6c250d9c135995308844673d9182fab
ThierryOrth <thierry.orth@gmail.com>
2022-09-27 15:29:54 +0200

    Data loading finished

--- data_loader.py
+++ data_loader.py
@@ -13,41 +13,15 @@ PIMA_DATA = np.genfromtxt(DIR+"/pima.txt", delimiter=",", skip_header=False)
 
 BUG_DATAFRAME_TRAIN = pd.read_csv("eclipse-metrics-packages-2.0.csv", delimiter=";")
 BUG_DATAFRAME_TEST = pd.read_csv("eclipse-metrics-packages-3.0.csv", delimiter=";")
+ 
+column_indices=[2]+[i for i in range(4, 44)]
+BUG_DATA_X_TRAIN = BUG_DATAFRAME_TRAIN.iloc[:,column_indices].to_numpy()
+BUG_DATA_Y_TRAIN = BUG_DATAFRAME_TRAIN.iloc[:,3].to_numpy()
 
-column_indices = [2]
-column_indices+=[i for i in range(4, 44)]
-BUG_DATA_Y_TRAIN = BUG_DATAFRAME_TRAIN.loc("post")
-BUG_DATA_Y_TEST = BUG_DATAFRAME_TEST.loc("post")
-# BUG_DATA_X_TRAIN = 
-# BUG_DATA_X_TEST =
-
-
-# for idx, column in enumerate(columns):
-#     print(idx, column)
-
-
-
-#ataFrame.to_numpy(
-
-
-44,210
-
-df.drop(df.columns[cols],axis=1,inplace=True)
-#df.drop(df.columns[i], axis=1)
-#BUG_DATA_TRAIN.drop()
-
-# POST BUGS AS TARGET
-
-# PRE BUGS AS FEATURE
-
-
-
-BUG_DATA_TEST = pd.read_csv("eclipse-metrics-packages-2.0.csv", delimiter=";")
-
-#BOTH CONSIST OF TUPLES FOR EACH FEATURE
+BUG_DATA_X_TEST = BUG_DATAFRAME_TEST.iloc[:,column_indices].to_numpy()
+BUG_DATA_Y_TEST = BUG_DATAFRAME_TEST.iloc[:,3].to_numpy
 
 if __name__ == "__main__":
     array=np.array([1,0,1,1,1,0,0,1,1,0,1])
     
-    print(BUG_DATA_TRAIN.shape)
-
+    print(BUG_DATA_X_TRAIN.shape)
