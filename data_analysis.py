97d37d3ce7865f31098283344af34644a6d24409
ThierryOrth <thierry.orth@gmail.com>
2022-10-06 21:16:00 +0200

    Cleaned up some code. Turns out that the results are not optimal, so we should find out tomorrow what the problem is.

--- data_analysis.py
+++ data_analysis.py
@@ -1,6 +1,8 @@
+from tkinter import N
 import numpy as np
 from data_loader import x_train, y_train, x_test, y_test
 from classification_tree_builder import tree_grow, tree_grow_b, tree_pred, tree_pred_b
+import sys
 
 def evaluation_metrics(y_true,y_pred,pos=1.0, neg=0.0):
     """Returns evaluation metrics given true
@@ -25,10 +27,13 @@ def evaluation_metrics(y_true,y_pred,pos=1.0, neg=0.0):
         return true_pos/(true_pos+false_pos+true_neg+false_neg)
 
     def precision(true_pos,false_pos):
-        return true_pos/(true_pos+false_pos)
+        return true_pos/(true_pos+false_pos) if 0<true_pos else 0.0
 
     def recall(true_pos,false_neg):
-        return true_pos/(true_pos+false_neg)
+        return true_pos/(true_pos+false_neg) if 0<true_pos else 0.0
+
+    def f1_score(precision, recall):
+        return 2*((precision*recall)/(precision+recall))
 
     return confusion_matrix(true_pos,false_pos,true_neg,false_neg), accuracy(true_pos,false_pos,true_neg,false_neg), \
                                      precision(true_pos,false_pos), recall(true_pos,false_neg)
@@ -37,20 +42,24 @@ def print_results(conf_matrix, acc, prec, rec, model_name):
     print(f"MODEL: {model_name} \n CONFUSION MATRIX: \n {conf_matrix} \n ACCURACY: {acc} \n PRECISION: {prec} \n RECALL: {rec} \n")
 
 if __name__ == "__main__":
-    n , _ = y_train.shape
-    tree = tree_grow(x_train, x_test, nmin=15, minleaf=5, nfeat=41)
-    tree_bagged = tree_grow_b(x_train, x_test, nmin=15, minleaf=5, nfeat=41, m=100)
-    random_forest = tree_grow_b(x_train, x_test, nmin=15, minleaf=5, nfeat=6, m=100) # set nfeat to 6 for analysis
+    n_of_obs, _  = y_train.shape
 
-    y_pred_n = [tree_pred(y_train[i, :], tree) for i in range(n)]
+    tree = tree_grow(x_train, x_test, nmin=15, minleaf=41, nfeat=41)
+    tree_bagged = tree_grow_b(x_train, x_test, nmin=15, minleaf=41, nfeat=41, m=100)
+    random_forest = tree_grow_b(x_train, x_test, nmin=15, minleaf=6, nfeat=6, m=100) # set nfeat to 6 for analysis
+
+    print(f"FIRST TWO SPLITS REGULAR TREE:\
+            {tree.root.feature_value, tree.root.left_child.feature_value,tree.root.right_child.feature_value} \n")
+
+    y_pred_n = [tree_pred(y_train[i, :], tree) for i in range(n_of_obs)]
     conf_matrix_n, acc_n, prec_n, rec_n = evaluation_metrics(y_test, y_pred_n)
     print_results(conf_matrix_n, acc_n, prec_n, rec_n, model_name="REGULAR CLASSIFICATION TREE")
 
-    y_pred_b = [tree_pred_b(y_train[i, :], tree_bagged) for i in range(n)]
+    y_pred_b = [tree_pred_b(y_train[i, :], tree_bagged) for i in range(n_of_obs)]
     conf_matrix_b, acc_b, prec_b, rec_b = evaluation_metrics(y_test, y_pred_b)
     print_results(conf_matrix_b, acc_b, prec_b, rec_b, model_name="BAGGED CLASSIFICATION TREE")
 
-    y_pred_f = [tree_pred_b(y_train[i, :], random_forest) for i in range(n)]
+    y_pred_f = [tree_pred_b(y_train[i, :], random_forest) for i in range(n_of_obs)]
     conf_matrix_f, acc_f, prec_f, rec_f = evaluation_metrics(y_test, y_pred_f)
     print_results(conf_matrix_f, acc_f, prec_f, rec_f, model_name="RANDOM FOREST")
     
--- data_loader.py
+++ data_loader.py
@@ -11,17 +11,17 @@ bug_dataframe_train = pd.read_csv("eclipse-metrics-packages-2.0.csv", delimiter=
 bug_dataframe_test = pd.read_csv("eclipse-metrics-packages-3.0.csv", delimiter=";")
  
 column_indices=[2]+[i for i in range(4, 44)]
+
+# PREPROCESSING STEP: SET BUG VALUES TO CATEGORICALS
 x_train = bug_dataframe_train.iloc[:,column_indices].to_numpy()
+x_train[:,0] = np.where(0<x_train[:,0], 1.0, 0.0)
+
 x_test = bug_dataframe_train.iloc[:,3].to_numpy()
+x_test = np.where(0<x_test, 1.0, 0.0)
 
 y_train = bug_dataframe_test.iloc[:,column_indices].to_numpy()
-y_test = bug_dataframe_test.iloc[:,3].to_numpy()
-
-# PREPROCESSING STEP: set pre and post to binary
-x_train[:,0] = np.where(0<x_train[:,0], 1.0, 0.0)
-x_test = np.where(0.0<x_test, 1.0, 0.0)
-
 y_train[:,0] = np.where(0<y_train[:,0], 1.0, 0.0)
+
+y_test = bug_dataframe_test.iloc[:,3].to_numpy()
 y_test = np.where(0.0<y_test, 1.0, 0.0)
 
-#print(x_train.shape,x_test.shape,y_train.shape,y_test.shape)
