848050272b51278a1ee7409ee8ec77cb863a4b18
ThierryOrth <thierry.orth@gmail.com>
2022-10-07 19:47:50 +0200

    Fixed the problem with random forest!

--- classification_trees.py
+++ classification_trees.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-import collections, os, graphviz, time
+import collections, os, graphviz, time, sys
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_fscore_support
 
 class Node:
@@ -11,8 +11,8 @@ class Node:
     def __init__(self):
         self.left_child = None
         self.right_child = None
-        self.feature_value = None 
-        self.split_threshold = None
+        self.feature = None 
+        self.threshold = None
         self.majority_label = None
         self.is_leaf = False
 
@@ -35,22 +35,25 @@ def tree_grow(feature_obs:np.array, class_obs:np.array, nmin:int, minleaf:int, n
 def tree_pred(feature_obs: np.array, tr: Tree) -> float:
     """"Traverses the tree by comparing feature value with split threshold until it finds a leaf. If that leaf is reached,
         we predict the majority_label vote."""
-    pred=[]
+
+    # never a negative classification value assigned in prediction
+    n_of_obs, _ = feature_obs.shape
+    pred = np.zeros(n_of_obs)
 
     # LOOP UNTIL WE REACH A LEAF NODE
-    for feature_row in feature_obs:
+
+    for obs_index in range(n_of_obs):
         current_node = tr.root
         while not current_node.is_leaf:
-            feature_value = int(current_node.feature_value)
+            feature_index = current_node.feature
 
-            if feature_row[feature_value] <= current_node.split_threshold:
+            if feature_obs[obs_index, feature_index] <= current_node.threshold:
                 current_node = current_node.left_child
             else:
                 current_node = current_node.right_child
 
         # APPEND MAJORITY LABEL OF LEAF NODE
-        pred.append(current_node.majority_label)
-
+        pred[obs_index] = current_node.majority_label
     return pred
 
 def tree_grow_b(feature_obs:np.array, class_obs:np.array, nmin:int, minleaf:int, nfeat:int, m:int) -> list:
@@ -60,6 +63,7 @@ def tree_grow_b(feature_obs:np.array, class_obs:np.array, nmin:int, minleaf:int,
     trees = []
 
     for i in range(m):
+        print(f"===EPOCH {i}===")
         root_node = Node()
         indices = np.random.choice(np.arange(0,n_of_obs,1), n_of_obs, replace=True)
         feat_sample, class_sample = feature_obs[indices,:], class_obs[indices]
@@ -76,56 +80,114 @@ def tree_pred_b(feature_obs: np.array, trs: list) -> float:
     col_index, _ = feature_obs.shape 
     predictions = np.zeros((row_index,col_index))
     
-    for tree_index, tree in enumerate(trs):
-        predictions[tree_index, :] = tree_pred(feature_obs,tree)
-    majority_labels = np.array([collections.Counter(predictions[:,i]).most_common(1)[0][0] for i in range(col_index)])
+    for index, tree in enumerate(trs):
+        predictions[index, :] = tree_pred(feature_obs,tree)    
+
+    majority_labels = [collections.Counter(predictions[:,i]).most_common(1)[0][0] for i in range(col_index)]
 
     return majority_labels
+
+# def extend_node(node:Node, x:np.array, y:np.array, nmin:int, minleaf:int, nfeat:int):
+#     """Given a current node, checks whether it can be extended. If not, the node becomes
+#                a leaf node. If so, then we split the node into two child nodes, which are fed into
+#                the same function by recursion."""
+#     feat_to_split = feat_to_imp = dict()
+#     n_of_obs, n_of_feat = x.shape
+#     indices = np.arange(0, n_of_feat, 1)
+#     node.majority_label = collections.Counter(y).most_common(1)[0][0]
+
+#     if n_of_obs < nmin or impurity(y) == 0:
+#         node.is_leaf=True
+#         return
+
+#     if nfeat<n_of_feat:
+#         indices = np.random.choice(np.arange(0,nfeat,1), nfeat, replace=False)
+#         n_of_feat = nfeat
+
+#     # MAP FEATURE INDICES TO SPLITS AND IMPURITY SCORES, GET FEATURE WITH MAXIMAL SPLIT
+#     for index in indices:
+#         split, imp = bestsplit(x[:, index], y, minleaf)  # best split for feature f in n_of_feat
+
+#         if split:
+#             feat_to_split[index] = split
+#             feat_to_imp[index] = imp
+
+#     if not feat_to_split:
+#         node.is_leaf = True
+#         return
+
+#     feature_index = min(feat_to_imp, key = feat_to_imp.get)    
+#     threshold = feat_to_split[feature_index]
+    
+#     # assign each row to either the left or right child based on the threshold
+#     l_feat_obs = x[x[:, feature_index] <= threshold]
+#     r_feat_obs = x[x[:, feature_index]> threshold]
+#     l_class_obs = y[x[:, feature_index] <= threshold]
+#     r_class_obs = y[x[:, feature_index] > threshold]
+
+#     node.feature = feature_index
+#     node.threshold = threshold
+#     node.left_child = node.right_child = Node()
+
+#     extend_node(node.left_child, l_feat_obs, l_class_obs, nmin, minleaf, nfeat)
+#     extend_node(node.right_child, r_feat_obs, r_class_obs, nmin, minleaf, nfeat)
 
 def extend_node(node:Node, x:np.array, y:np.array, nmin:int, minleaf:int, nfeat:int):
     """Given a current node, checks whether it can be extended. If not, the node becomes
                a leaf node. If so, then we split the node into two child nodes, which are fed into
                the same function by recursion."""
-    feat_to_split, feat_to_imp = dict(), dict()
+    feat_to_split = dict()
+    feat_to_imp = dict()
     num_of_obs, n_of_feat = x.shape
-    majority_label = collections.Counter(y).most_common(1)[0][0]
-    node.majority_label = majority_label
 
     if nfeat<n_of_feat:
         indices = np.random.choice(np.arange(0,nfeat,1), nfeat, replace=False)
         x = x[:, indices]
         n_of_feat = nfeat
 
-    # MAP FEATURE INDICES TO SPLITS AND IMPURITY SCORES, GET FEATURE WITH MAXIMAL SPLIT
-    for index in range(n_of_feat):
-        split, imp = bestsplit(x[:, index], y,minleaf)  # best split for feature f in n_of_feat
+    # Calculate the best split option for every feature in array x
 
+    majority_label = collections.Counter(y).most_common(1)[0][0]
+    node.majority_label = majority_label
+
+    for index in range(n_of_feat):
+        split, imp = bestsplit(x[:,index],y,minleaf)
+        
         if split:
-            feat_to_split[index] = split
-            feat_to_imp[index] = imp
+            feat_to_split[index]=split
+            feat_to_imp[index]=imp
 
     if not feat_to_split or num_of_obs < nmin or impurity(y) == 0:
         node.is_leaf = True
         return
 
-    feature_index = max(feat_to_imp, key = lambda x : x)    
-    feature_matrix = x[:, feature_index]
+    feature_index = min(feat_to_imp, key=feat_to_imp.get)
     threshold = feat_to_split[feature_index]
-
-    node.feature_value = feature_index
-    node.split_threshold = threshold
     
-    # assign each row to either the left or right child based on the threshold
-    l_feat_obs = x[feature_matrix <= threshold]
-    r_feat_obs = x[feature_matrix > threshold]
+    feature_values = x[:, feature_index]
 
-    l_class_obs = y[feature_matrix <= threshold]
-    r_class_obs = y[feature_matrix > threshold]
+    node.feature = feature_index
+    node.threshold = threshold
 
-    node.left_child = node.right_child = Node()
+    # check for all the best splits of all features which one
+    # of these has the best impurity reduction
 
-    extend_node(node.left_child, l_feat_obs, l_class_obs, nmin, minleaf, nfeat)
-    extend_node(node.right_child, r_feat_obs, r_class_obs, nmin, minleaf, nfeat)
+
+    # MAKE A LEAF NODE IF THERE EXISTS NO SPLIT, THE NUMBERS OF
+    # OBSERVATIONS IS TOO LOW OR IMPURITY EQUALS ZERO
+
+    # assign each row to either the left or right child based on its value
+    left = x[x[:, feature_index] <= threshold]
+    right = x[x[:, feature_index] > threshold]
+
+    left_labels = y[feature_values <= threshold]
+    right_labels = y[feature_values > threshold]
+
+    node.left_child = Node()
+    node.right_child = Node()
+
+    extend_node(node.left_child, left, left_labels, nmin, minleaf, nfeat)
+    extend_node(node.right_child, right, right_labels, nmin, minleaf, nfeat)
 
 
 def impurity(class_obs: np.array) -> float:
@@ -136,35 +198,33 @@ def impurity(class_obs: np.array) -> float:
     neg_class = class_obs[class_obs == 0].size
     return (pos_class / total) * (neg_class / total)
 
+
 def bestsplit(x,y,minleaf):
     """Finds the best split from a range of candidate splits
             using the Gini index as impurity function."""
-    highest_red = 0
+    best_imp = 1
     x_sorted = np.sort(np.unique(x))
-    candidate_splitpoints = (x_sorted[0:(x_sorted.size - 1)] + x_sorted[1:x_sorted.size]) / 2
+    cand_splits = (x_sorted[0:(x_sorted.size - 1)] + x_sorted[1:x_sorted.size]) / 2
     best_split = None  # IF THIS VALUE IS RETURNED, THEN WE KNOW THAT THERE EXISTS NO POSSIBLE SPLIT
 
     ## for every split option calculate the gini index.
-    for split in candidate_splitpoints:
+    for split in cand_splits:
         left_child = y[x <= split]
         right_child = y[x > split]
 
         # MAKE A LEAF NODE IF THE NUMBER OF OBSERVATIONS
         # FOR EITHER CHILD NODE IS TOO LOW
-        if minleaf <= len(left_child) or  minleaf <= len(right_child):
-            imp_left = impurity(left_child)
-            imp_right = impurity(right_child)
+        if minleaf <= len(left_child) and minleaf <= len(right_child):
             pi_left = len(left_child) / len(x)
-            pi_right = len(right_child) / len(x)
-            imp_parent = impurity(y)
-            red = imp_parent - ((pi_left * imp_left) + (pi_right * imp_right))
+            pi_right = len(right_child) / len(x) 
+            imp = impurity(y) - ((pi_left * impurity(left_child)) + (pi_right * impurity(right_child)))
 
-            if highest_red < red:
-                highest_red = red
+            if imp<best_imp:
+                best_imp = imp
                 best_split = split
 
     # return the best split option with the corresponding impurity reduction
-    return best_split, highest_red
+    return best_split, best_imp
 
 def load_data(dataset):
     dir = os.getcwd()
@@ -196,7 +256,7 @@ def load_data(dataset):
         y_test = bug_dataframe_test.iloc[:,3].to_numpy()
         y_test = np.where(0.0<y_test, 1.0, 0.0)
 
-        return x_train,x_test,y_train,y_test
+        return x_train, x_test, y_train, y_test
 
 def print_results(y_true,y_pred, name, runtime, verbose=False):
     scores = precision_recall_fscore_support(y_true,y_pred)
@@ -214,24 +274,21 @@ def print_results(y_true,y_pred, name, runtime, verbose=False):
             file.write(string)
 
 def visualize_tree(node_names, edge_list, name_to_threshold):
+    #TODO
     dot = graphviz.Digraph(comment="first two splits classification tree")
     
-
     for node_name in node_names:
         dot.node(node_name, str(name_to_threshold[node_name]))
     dot.edges(edge_list)
 
-    #TODO
-
-
 if __name__ == "__main__":
     np.random.seed(42)
-    x_train,x_test,y_train,y_test = load_data(dataset="bug")
+    x_train, x_test, y_train, y_test = load_data(dataset="bug")
 
-    # start_time = time.time()
-    # tree = tree_grow(x_train, x_test, nmin=15, minleaf=5, nfeat=41)
-    # y_pred_n = tree_pred(y_train, tree)
-    # print_results(y_test,y_pred_n, name="regular_tree", runtime=time.time() - start_time,verbose=True)
+    start_time = time.time()
+    tree = tree_grow(x_train, x_test, nmin=15, minleaf=5, nfeat=41)
+    y_pred_n = tree_pred(y_train, tree)
+    print_results(y_test, y_pred_n, name="regular_tree", runtime=time.time() - start_time,verbose=True)
 
     # A picture of the first two splits of the single tree (the split in the root
     # node, and the split in its left or right child). Consider the classification
@@ -241,11 +298,10 @@ if __name__ == "__main__":
 
     #visualize_tree(node_names= ,edge_list=, name_to_threshold=,)
 
-
-    # start_time = time.time()
-    # tree_bagged = tree_grow_b(x_train, x_test, nmin=15, minleaf=5, nfeat=41, m=100)
-    # y_pred_b = tree_pred_b(y_train, tree_bagged)
-    # print_results(y_test,y_pred_b, name="bagged_tree", runtime=time.time() - start_time,verbose=True)
+    start_time = time.time()
+    tree_bagged = tree_grow_b(x_train, x_test, nmin=15, minleaf=5, nfeat=41, m=100)
+    y_pred_b = tree_pred_b(y_train, tree_bagged)
+    print_results(y_test,y_pred_b, name="bagged_tree", runtime=time.time() - start_time,verbose=True)
 
     start_time = time.time()
     random_forest = tree_grow_b(x_train, x_test, nmin=15, minleaf=5, nfeat=6, m=100)
