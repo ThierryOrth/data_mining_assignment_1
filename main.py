be8d07519384c055eef96e542d2db76b40d002ba
ThierryOrth <79374943+ThierryOrth@users.noreply.github.com>
2022-10-07 14:28:10 +0200

    update data

--- main.py
+++ main.py
@@ -182,6 +182,26 @@ def gini_index(arr):
     return gini
 
 if __name__ == "__main__":
+    
+    bug_dataframe_train = pd.read_csv("eclipse-metrics-packages-2.0.csv", delimiter=";")
+    bug_dataframe_test = pd.read_csv("eclipse-metrics-packages-3.0.csv", delimiter=";")
+
+    column_indices=[2]+[i for i in range(4, 44)]
+
+
+    # PREPROCESSING STEP: SET BUG VALUES TO CATEGORICALS
+    x_train = bug_dataframe_train.iloc[:,column_indices].to_numpy()
+    x_train[:,0] = np.where(0<x_train[:,0], 1.0, 0.0)
+
+    x_test = bug_dataframe_train.iloc[:,3].to_numpy()
+    x_test = np.where(0<x_test, 1.0, 0.0)
+
+    y_train = bug_dataframe_test.iloc[:,column_indices].to_numpy()
+    y_train[:,0] = np.where(0<y_train[:,0], 1.0, 0.0)
+
+    y_test = bug_dataframe_test.iloc[:,3].to_numpy()
+    y_test = np.where(0.0<y_test, 1.0, 0.0)
+
     #num_of_obs, n_of_feat = pima_data.shape
     #X = pima_data[:,:n_of_feat-1]
     #Y = pima_data[:,n_of_feat-1]
